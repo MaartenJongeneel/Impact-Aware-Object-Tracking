92648fa396b59c8082139f8837fcd7fd1a7a0a2b
s140067 <m.j.jongeneel@tue.nl>
2022-10-19 09:27:07 +0200

    Updated hat function

--- BoxSim.cpp
+++ BoxSim.cpp
@@ -57,45 +57,25 @@ double BoxSim::proxCN(double x){
 
 //Hat operator
 void BoxSim::hat(double iets[], int size){
-    std::cout << "Length of array = " << size << std::endl;
-
     if(size ==3){
         matrix res(3, vec(3));
-        res[0][0] = 0;
-        res[0][1] = -iets[2];
-        res[0][2] = iets[1];
-        res[1][0] = iets[2];
-        res[1][1] = 0;
-        res[1][2] = -iets[0];
-        res[2][0] = -iets[1];
-        res[2][1] = iets[0];
-        res[2][2] = 0;
+        res[0][0] = 0;        res[0][1] = -iets[2];  res[0][2] = iets[1];  
+        res[1][0] = iets[2];  res[1][1] = 0;         res[1][2] = -iets[0];  
+        res[2][0] = -iets[1]; res[2][1] = iets[0];   res[2][2] = 0;
         print(res);
     }
     else if(size==6){
         matrix res(4, vec(4));
-        res[0][0] = 0;
-        res[0][1] = -iets[5];
-        res[0][2] = iets[4];
-        res[1][0] = iets[5];
-        res[1][1] = 0;
-        res[1][2] = -iets[3];
-        res[2][0] = -iets[4];
-        res[2][1] = iets[3];
-        res[2][2] = 0;
-        res[0][3] = iets[0];
-        res[1][3] = iets[1];
-        res[2][3] = iets[2];
-        res[3][0] = 0;
-        res[3][1] = 0;
-        res[3][2] = 0;
-        res[3][3] = 0;
+        res[0][0] = 0;         res[0][1] = -iets[5];  res[0][2] = iets[4];   res[0][3] = iets[0];
+        res[1][0] = iets[5];   res[1][1] = 0;         res[1][2] = -iets[3];  res[1][3] = iets[1];
+        res[2][0] = -iets[4];  res[2][1] = iets[3];   res[2][2] = 0;         res[2][3] = iets[2];
+        res[3][0] = 0;         res[3][1] = 0;         res[3][2] = 0;         res[3][3] = 0;
         print(res);
     };
     
 }
 
-
+// Print matrix to the screen
 void BoxSim::print(matrix const& M) {
     size_t n = M.size();
     for(size_t i = 0; i < n; ++i) {
@@ -106,13 +86,3 @@ void BoxSim::print(matrix const& M) {
 }
 
 
-// function res = hat(vec)
-// % Take the 3- or 6-vector representing an isomorphism of so(3) or se(3) and writes this as element of so(3) or se(3). 
-// if length(vec) == 3
-//     res = [0, -vec(3), vec(2); vec(3), 0, -vec(1); -vec(2), vec(1), 0];
-// elseif length(vec) == 6
-//     skew = [0, -vec(6), vec(5); vec(6), 0, -vec(4); -vec(5), vec(4), 0];
-//     v = [vec(1);vec(2);vec(3)];
-//     res = [skew, v; zeros(1,4)];
-// end
-// end
